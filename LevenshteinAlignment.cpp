f0f6744a00a57cb86a31343a684c293fff222d9b
root <root@hif-dev-srec02-vm002.internal.cloudapp.net>
2020-09-11 13:26:17 +0900

    버퍼 클리어 버그 수정

--- LevenshteinAlignment.cpp
+++ LevenshteinAlignment.cpp
@@ -129,18 +129,42 @@ public:
 		itb = bottomAlignmentRowVec.end();
 		itb--;
 		bottomAlignmentRowVec.erase(itb);
-        */
-		for(int i = topAlignmentRowVec.size(); i >= 0 ; i--)
+        
+		for(int i = topAlignmentRowVec.size() - 1; i >= 0 ; i--)
 			printf("%s\t",topAlignmentRowVec[i].c_str());
 		printf("\n");
-		for(int i = editSequenceVec.size(); i >= 0; i--)
+		for(int i = editSequenceVec.size() - 1; i >= 0; i--)
 			printf("%d\t",editSequenceVec[i]);
 		printf("\n");
-		for(int i = bottomAlignmentRowVec.size(); i >= 0; i--)
+		for(int i = bottomAlignmentRowVec.size() - 1; i >= 0; i--)
 			printf("%s\t",bottomAlignmentRowVec[i].c_str());
 		printf("\n");
+        */
 
 	}
+    int getDistance()
+    {
+        int dist = 0;
+        for(int i = editSequenceVec.size() - 1; i >= 0; i--){
+			int ret = editSequenceVec[i];
+            if(ret != 3)
+                dist++;
+        }
+        return dist;
+    }
+    void getRefAlignmentRowVec(vector<string>& tbl){
+        for(int i = topAlignmentRowVec.size()-1; i >= 0; i--)
+            tbl.push_back(topAlignmentRowVec[i]);           
+    }
+    void getEditSequenceVec(vector<string>& tbl){
+		for(int i = editSequenceVec.size() - 1; i >= 0; i--)
+
+			tbl.push_back(getEditResult2String(editSequenceVec[i]));        
+    }
+    void getTrgAlignmentRowVec(vector<string>& tbl){
+        for(int i = bottomAlignmentRowVec.size() - 1; i >=0; i--)
+            tbl.push_back(bottomAlignmentRowVec[i]);
+    }
 
 
 private:
@@ -148,6 +172,28 @@ private:
 	vector<string> topAlignmentRowVec;
 	vector<string> bottomAlignmentRowVec;
 	vector<EditOperation> editSequenceVec;
+
+    string getEditResult2String(int num)
+    {
+        switch(num)
+        {
+            case 0:
+                return "I";
+                break;
+            case 1:
+                return "S";
+                break;
+            case 2:
+                return "D";
+                break;
+            case 3:
+                return "N";
+                break;
+            default:
+                return "-";
+                break;
+        }
+    }
 
 };
 // char 배열의 한글을 string type의 벡터로
@@ -177,19 +223,67 @@ int split(const char* pstr, vector<string>& tbl)
 
 int main()
 {
-	const char* psrc="나는사랑합니다";
-	const char* ptrg="너는사당합다요";
+	const char* psrc="너무나무가많다고생각합니다";
+	const char* ptrg="무나무가없다고이야기를합니다";
 
 	vector<string> stbl;
 	vector<string> ttbl;
 
 	split(psrc,stbl);
 	split(ptrg,ttbl);
-	
-	
-	
+
 	LevenshteinEditDistance led;
 	led.compute(stbl,ttbl);
-	return 1;
 
+    int dist = led.getDistance();
+
+    vector<string> refTbl;
+    vector<string> trgTbl;
+    vector<string> editTbl;
+
+    led.getRefAlignmentRowVec(refTbl);
+    led.getTrgAlignmentRowVec(trgTbl);
+    led.getEditSequenceVec(editTbl);
+
+    int nInsert = 0;
+    int nSubstitute = 0;
+    int nDelete = 0;
+    int nNormal = 0;
+    int nTotal = 0;
+    float fCorr = 0.0;
+    for(int i = 0; i < editTbl.size(); i++){
+        string s = editTbl[i];
+        if(s.compare("N")==0)
+            nNormal++;
+        else if(s.compare("S")==0)
+            nSubstitute++;
+        else if(s.compare("D")==0)
+            nDelete++;
+        else if(s.compare("I")==0)
+            nInsert++;
+    }
+    nTotal = nNormal+nSubstitute+nDelete+nInsert;
+    fCorr = (float)(nSubstitute+nDelete+nInsert)/(float)nTotal;
+
+
+    printf("\n============== Speech Recognition Result Analysis ==============\n");
+    printf("참조문장:\t");
+    for(int i = 0; i < refTbl.size(); i++)
+        printf("%s\t",refTbl[i].c_str());
+    printf("\n");
+    printf("비교결과:\t");
+    for(int i = 0; i < editTbl.size(); i++)
+        printf("%s\t",editTbl[i].c_str());
+    printf("\n");
+    printf("인식문장:\t");
+    for(int i = 0; i < trgTbl.size(); i++){
+        printf("%s\t",trgTbl[i].c_str());
+    }
+    printf("\n");
+    printf("--------------------------- Overall Result -------------------------\n");
+    printf("문자:\tCorr=%3.2f\t",fCorr);
+    printf("[H=%d,\tD=%d,\tS=%d,\tI=%d\t,N=%d]",nTotal,nDelete,nSubstitute,nInsert,nNormal);
+    printf("\n====================================================================\n");
+    
+    return 1;
 }
